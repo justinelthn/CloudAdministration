7bf587741fecbedb22a855535b3d12ddc218dd42
Justine Le Théno <justine.lthn@gmail.com>
2026-02-19 16:27:09 +0100

    ajout frontend + changes

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+<<<<<<< HEAD
 	"movie-api/handlers"
 
 	"github.com/gin-gonic/gin"
@@ -18,4 +19,21 @@ func main() {
 	r.GET("/movies", handlers.GetMovies)
 
 	r.Run(":8082")
+=======
+    "log"
+
+    "github.com/gin-gonic/gin"
+
+    "movie-api/handlers" // <<== ici on met ton module + dossier
+)
+
+func main() {
+    r := gin.Default()
+
+    r.GET("/movies", handlers.GetMovies)
+    r.GET("/suggest", handlers.Suggest)
+
+    log.Println("Server running on :8081")
+    r.Run(":8081")
+>>>>>>> 51699d5 (ajout frontend + changes)
 }
