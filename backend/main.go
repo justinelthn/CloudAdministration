d2e778d48137bb37e3788a5f4af92f7346a15cb5
Jaufré Hacquard <jaufre.hacquard@edu.devinci.fr>
2026-03-13 15:48:10 +0100

    c'est prêt

--- backend/main.go
+++ backend/main.go
@@ -1,35 +1,8 @@
 package main
 
 import (
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	"movie-api/handlers"
-=======
-    "log"
->>>>>>> 828618a7bbb67b386c74c4f6ba8dc2b56d6f99ca
-
-    "github.com/gin-gonic/gin"
-
-    "movie-api/handlers" // <<== ici on met ton module + dossier
-)
-
-func main() {
-    r := gin.Default()
-
-    r.GET("/movies", handlers.GetMovies)
-    r.GET("/suggest", handlers.Suggest)
-
-<<<<<<< HEAD
-	r.GET("/movies", handlers.GetMovies)
-
-	r.Run(":8082")
-=======
-    "log"
-=======
 	"log"
 	"time"
->>>>>>> 4ae80e220b143f9feb209044a235bde9189a26e7
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -50,15 +23,6 @@ func main() {
 		MaxAge:           12 * time.Hour,
 	}))
 
-<<<<<<< HEAD
-    log.Println("Server running on :8081")
-    r.Run(":8081")
->>>>>>> 51699d5 (ajout frontend + changes)
-=======
-    log.Println("Server running on :8081")
-    r.Run(":8081")
->>>>>>> 828618a7bbb67b386c74c4f6ba8dc2b56d6f99ca
-=======
 	r.GET("/movies", handlers.GetMovies)
 	r.GET("/suggest", handlers.Suggest)
 
@@ -69,5 +33,4 @@ func main() {
 
 	log.Println("Server running on :8081")
 	r.Run(":8081")
->>>>>>> 4ae80e220b143f9feb209044a235bde9189a26e7
 }
